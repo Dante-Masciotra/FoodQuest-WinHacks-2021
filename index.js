4299c9b28f01b6eed5f14c46dfea3dfa942001ca
Dante <jazzy.d1302@outlook.com>
2021-03-26 21:23:26 -0400

    Dynamic

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function create_info(){
+    var info = {Name:""};
+    info.Name="World"
+    var restaurant = document.createElement('div');
+    restaurant.id = "r";
+    restaurant.className -"r";
+    document.getElementById("restaurants").appendChild(restaurant);
+    restaurant.innerHTML= info.Name;
+}
