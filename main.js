08e5dee6dcf6a4127cf494b2726bdf264c18fa67
Dante <jazzy.d1302@outlook.com>
2021-03-28 07:33:27 -0400

    distance and dollar sign

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ var myStorage = window.sessionStorage;
 var lat = myStorage.getItem("Lat");
 var lng = myStorage.getItem("Lng");
 var radius= myStorage.getItem("rad");
+var myLatLng={ lat: 0, lng: 0 };
 
 function create_info(name, rating,price,distance){
 
@@ -28,6 +29,11 @@ function create_info(name, rating,price,distance){
     rPrice.id = "rPrice";
     rPrice.className ="r";
 
+    var img = document.createElement('img');
+    img.id = "rimg";
+    img.className ="r";
+    img.src="img/Dollar sign.png"
+
     var rDist = document.createElement('div');
     rDist.id = "rDist";
     rDist.className ="r";
@@ -38,18 +44,20 @@ function create_info(name, rating,price,distance){
     Rest.appendChild(rName);
     Rest.appendChild(rRating);
     Rest.appendChild(rPrice);
+    for(var i=0; i<price; i++){
+      rPrice.appendChild(img);
+    }
     Rest.appendChild(rDist);
 
     rName.innerHTML= name;
     rRating.innerHTML= rating;
-    rPrice.innerHTML= price;
     rDist.innerHTML= distance;
 }
 
 
 
 function initMap() {
-    const myLatLng = { lat: parseFloat(lat), lng: parseFloat(lng) };
+    myLatLng = { lat: parseFloat(lat), lng: parseFloat(lng) };
     infowindow = new google.maps.InfoWindow();
     map = new google.maps.Map(document.getElementById("map"), {
       zoom: 15,
@@ -62,17 +70,34 @@ function initMap() {
     };
 
     var service = new google.maps.places.PlacesService(map);
+    var service2 = new google.maps.DistanceMatrixService();
     service.nearbySearch(request, (results, status) => {
         console.log(results);
         if (status === google.maps.places.PlacesServiceStatus.OK && results) {
           for (let i = 0; i < results.length; i++) {
-            createMarker(results[i],i);
+            service2.getDistanceMatrix({
+              origins: [myLatLng],
+              destinations: [results[i].vicinity],
+              travelMode: 'DRIVING'
+            },(response, status) => {
+              if (status !== "OK") {
+                alert("Error was: " + status);
+              } else {
+                console.log(response);
+                createMarker(results[i],response.rows[0].elements[0].distance.text);
+              }
+          });
+            
+            
           }
         }
       });
+      
+      
+      
   }
 
-  function createMarker(place,i) {
+  function createMarker(place,dist) {
     if (!place.geometry || !place.geometry.location) return;
     const marker = new google.maps.Marker({
       map,
@@ -84,7 +109,7 @@ function initMap() {
     console.log(place);
     if(lStorage.getItem(place.name)!=null){
       if(place.business_status == "OPERATIONAL"){
-        create_info(place.name, place.rating,place.price_level,0);
+        create_info(place.name, place.rating,place.price_level,dist);
       }else if(place.business_status != "OPERATIONAL"){
         create_info(place.name, place.rating,place.price_level,0);
       }
@@ -140,4 +165,8 @@ function call_Test(){
       }
     },
   });
+}
+
+function get_dist(origin,dest){
+
 }
